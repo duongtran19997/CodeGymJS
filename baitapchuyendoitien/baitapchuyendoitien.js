138741831f7f7d311a7374d8c525d6f449c5c289
superman <duonga2qp@gmail.com>
2022-03-27 20:40:11 +0700

    nopbai

---
+++ baitapchuyendoitien/baitapchuyendoitien.js
@@ -0,0 +1,5 @@
+ let x = document.getElementById("sotien")
+ let y = document.getElementById("y")
+ function button() {
+  document.getElementById("y").innerText = parseFloat(document.getElementById("x").value) / 23000;
+ }
