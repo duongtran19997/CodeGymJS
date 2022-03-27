2cb30fae877bbbecd51c9df7ab81fedf788abd8c
superman <duonga2qp@gmail.com>
2022-03-27 16:39:23 +0700

    nopbai

---
+++ baithuchanhtoantu/toantu.js
@@ -0,0 +1,6 @@
+let a = prompt("input the width")
+let b = prompt("input the height")
+    let width = parseInt(a)
+    let height = parseInt(b)
+let area = width * height
+document.write("area =" + area)
