b8ede75e2ed075514ab4d758ebe055ec3c2c7797
superman <duonga2qp@gmail.com>
2022-03-28 20:54:20 +0700

    nopbai

---
+++ baitapungdungmaytinh/maytinh2.js
@@ -0,0 +1,11 @@
+    function setOutput(value) {
+    document.getElementById('input').value += value
+    }
+    function calculator(){
+    let input = document.getElementById('input');
+    result = eval('input')
+        document.getElementById('input').value= result
+    }
+    function clear(){
+        document.getElementById('input').value = '';
+    }
