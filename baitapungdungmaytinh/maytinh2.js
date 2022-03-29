e04929d9eb9c94154a33b57d980242ae9333f128
superman <duonga2qp@gmail.com>
2022-03-29 09:13:28 +0700

    nopbai

--- baitapungdungmaytinh/maytinh2.js
+++ baitapungdungmaytinh/maytinh2.js
@@ -2,9 +2,9 @@
     document.getElementById('input').value += value;
     }
     function calculator(){
-    let input = document.getElementById('input');
-     result = eval('input');
-        document.getElementById('input').value = result;
+    let input = document.getElementById('input').value;
+      let result= eval(input)
+       document.getElementById('input').value = result;
     }
     function clear(){
         document.getElementById('input').value = '';
