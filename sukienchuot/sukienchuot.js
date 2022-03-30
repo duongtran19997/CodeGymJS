4926c7bd2b76c1fc12ad5b05640df9d1346b03c8
superman <duonga2qp@gmail.com>
2022-03-30 14:08:39 +0700

    nopbai

---
+++ sukienchuot/sukienchuot.js
@@ -0,0 +1,12 @@
+
+let img;
+function imgobj(){
+    img = document.getElementById('myImage')  ;
+    img.style.position = 'relative';
+    img.style.left = '0px'
+}
+
+function movefunction(){
+    img.style.left = parseInt(img.style.left)+10+'px'
+}
+window.onload = imgobj;
